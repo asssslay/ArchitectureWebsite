9afbaf33b55e0bd1fc5a0195a184826bb114a9c2
asssslay <avramova.ruslana.a.r.r@gmail.com>
2024-05-22 14:12:06 +0300

    gitignore

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import "../styles/modern-normal.css";
+import "../styles/style.css";
+import "../styles/components/header.css";
+import "../styles/components/hero.css";
+import "../styles/components/about.css";
+import "../styles/components/features.css";
+import "../styles/components/projects.css";
+import "../styles/components/contact.css";
+import "../styles/components/footer.css";
+import "../styles/components/gallery/gallery.css";
+import "../styles/utility.css";
+
+// import btnSlider from "./btn-slider";
+
+// btnSlider();
