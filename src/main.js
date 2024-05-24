70f3b1e798e47134657d9b8d36d61e21e6b69c27
asssslay <avramova.ruslana.a.r.r@gmail.com>
2024-05-24 13:41:08 +0300

    Add projects page

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import "../styles/components/header.css";
 import "../styles/components/hero.css";
 import "../styles/components/about.css";
 import "../styles/components/features.css";
-import "../styles/components/projects.css";
+import "../styles/components/our-projects.css";
 import "../styles/components/contact.css";
 import "../styles/components/footer.css";
 import "../styles/components/gallery/gallery.css";
