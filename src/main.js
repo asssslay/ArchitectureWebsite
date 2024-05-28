640c4c373bbb8b2097dd5527c8d8d8bb05654819
asssslay <avramova.ruslana.a.r.r@gmail.com>
2024-05-28 16:10:20 +0300

    Refactored project structure

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 import "../styles/modern-normal.css";
 import "../styles/style.css";
 import "../styles/components/header.css";
-import "../styles/components/hero.css";
-import "../styles/components/about.css";
-import "../styles/components/features.css";
-import "../styles/components/our-projects.css";
-import "../styles/components/contact.css";
+import "../styles/components/main/hero.css";
+import "../styles/components/main/about.css";
+import "../styles/components/main/features.css";
+import "../styles/components/main/our-projects.css";
+import "../styles/components/main/contact.css";
 import "../styles/components/footer.css";
 import "../styles/components/gallery/gallery.css";
 import "../styles/components/projects/projects.css";
-import "../styles/components/projects/project1.css";
+import "../styles/components/projects/sample-project.css";
 import "../styles/utility.css";
 
 // import btnSlider from "./btn-slider";
